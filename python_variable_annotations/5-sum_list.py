8693aae4be1c3f11e4984ce630577c6ce03e7612
oluwabunmi2 <o.adebanjo@alustudent.com>
2024-01-10 13:03:15 +0000

    annotations

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Complex types - list of floats """
+import typing
+
+def sum_list(input_list: List[float]) -> float:
+    """
+     def sum_list(input_list: List[float]) -> float:
+    """
+    Takes a list input_list of floats as argument
+
+    returns their sum as a float.
+    """
+
+    return sum(input_list)
