4152d69a1591aee52c5df62c734c17ddd5aca8b5
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2024-03-30 12:04:04 +0100

    map data structure

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
-function groceriesList() {
-  const groceries = new Map();
-  groceries.set('Apples', 10);
-  groceries.set('Tomatoes', 10);
-  groceries.set('Pasta', 1);
-  groceries.set('Rice', 1);
-  groceries.set('Banana', 5);
-  return groceries;
-}
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
 
-export default groceriesList;
+  return new Map(values);
+}
